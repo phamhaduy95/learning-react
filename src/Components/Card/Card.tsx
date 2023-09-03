f76c7c253c677f8657013f7479fb7fd3efb579a3
phamhaduy95 <ph.duy113@gmail.com>
2023-09-03 17:51:47 +0700

    reformat code

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -12,14 +12,19 @@ export type CardProps = {
   Note: Export only one component per component file to ensure HRM operates correctly.
 */
 
-export const Card: React.FC<CardProps> = ({ className = '', Header = <></>, Body = <></>, Footer = <></> }) => {
+export const Card: React.FC<CardProps> = ({
+    className = '',
+    Header = <></>,
+    Body = <></>,
+    Footer = <></>,
+}) => {
     const rootClassName = classNames('Card', className);
 
     return (
         <div className={rootClassName}>
-            <div className='Card__Header'>{Header}</div>
-            <div className='Card__Body'>{Body}</div>
-            <div className='Card__Footer'>{Footer}</div>
+            <div className="Card__Header">{Header}</div>
+            <div className="Card__Body">{Body}</div>
+            <div className="Card__Footer">{Footer}</div>
         </div>
     );
 };
