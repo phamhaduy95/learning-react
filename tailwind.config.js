591e91ced2bceff87a5953e3bcd5bb4e4c5cfa50
phamhaduy95 <ph.duy113@gmail.com>
2023-09-03 15:39:27 +0700

    add postcss and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                sm: '480px',
+                md: '768px',
+                lg: '976px',
+                xl: '1440px',
+            },
+            colors: {
+                primary: {
+                    text: '#8492a6',
+                    bg: '',
+                },
+                secondary: {
+                    background: '#f0f3f599',
+                },
+            },
+        },
     },
     plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css:{modules:{
-    localsConvention:"camelCaseOnly"
-  }}
-})
+    plugins: [react(), tsconfigPaths()],
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly',
+        },
+    },
+});
